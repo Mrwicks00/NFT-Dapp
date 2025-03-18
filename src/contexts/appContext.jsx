0e152f757f7a753752e28b90a66dd18d43a678c8
AjayiMike <ajayimikeofficial@gmail.com>
2025-03-18 19:38:37 +0100

    feat: add multicall feature

--- src/contexts/appContext.jsx
+++ src/contexts/appContext.jsx
@@ -1,178 +1,245 @@
-import { Contract } from "ethers";
+import { Contract, Interface } from "ethers";
 import { createContext, useContext, useEffect, useState } from "react";
 import { getReadOnlyProvider } from "../utils";
 import NFT_ABI from "../ABI/nft.json";
+import MULTCALL_ABI from "../ABI/multicall2.json";
 import { useAccount, useConfig, WagmiConfig } from "wagmi";
 import { getEthersSigner } from "../config/wallet-connection/adapter";
 
 const appContext = createContext();
 
 export const useAppContext = () => {
-  const context = useContext(appContext);
-  if (!context) {
-    throw new Error("useAppContext must be used within an AppProvider");
-  }
+    const context = useContext(appContext);
+    if (!context) {
+        throw new Error("useAppContext must be used within an AppProvider");
+    }
 
-  return context;
+    return context;
 };
 
 export const AppProvider = ({ children }) => {
-  const { address: userAddress, isConnected } = useAccount();
-  const wagmiConfig = useConfig();
-  const [nextTokenId, setNextTokenId] = useState(null);
-  const [maxSupply, setMaxSupply] = useState(null);
-  const [baseTokenURI, setBaseTokenURI] = useState("");
-  const [tokenMetaData, setTokenMetaData] = useState(new Map());
-  const [mintPrice, setMintPrice] = useState(null);
-  const [userNFTs, setUserNFTs] = useState([]);
+    const { address: userAddress, isConnected } = useAccount();
+    const wagmiConfig = useConfig();
+    const [nextTokenId, setNextTokenId] = useState(null);
+    const [maxSupply, setMaxSupply] = useState(null);
+    const [baseTokenURI, setBaseTokenURI] = useState("");
+    const [tokenMetaData, setTokenMetaData] = useState(new Map());
+    const [mintPrice, setMintPrice] = useState(null);
+    const [userNFTs, setUserNFTs] = useState([]);
 
-  useEffect(() => {
-    const contract = new Contract(
-      import.meta.env.VITE_NFT_CONTRACT_ADDRESS,
-      NFT_ABI,
-      getReadOnlyProvider()
-    );
-    contract
-      .nextTokenId()
-      .then((id) => setNextTokenId(id))
-      .catch((error) => console.error("error: ", error));
+    useEffect(() => {
+        const fetchContractData = async () => {
+            try {
+                const contractInterface = new Interface(NFT_ABI);
+                const multicall = new Contract(
+                    import.meta.env.VITE_MULTICALL_CONTRACT_ADDRESS,
+                    MULTCALL_ABI,
+                    getReadOnlyProvider()
+                );
 
-    contract
-      .baseTokenURI()
-      .then((uri) => setBaseTokenURI(uri))
-      .catch((error) => console.error("error: ", error));
+                const calls = [
+                    {
+                        target: import.meta.env.VITE_NFT_CONTRACT_ADDRESS,
+                        callData: contractInterface.encodeFunctionData(
+                            "nextTokenId",
+                            []
+                        ),
+                    },
+                    {
+                        target: import.meta.env.VITE_NFT_CONTRACT_ADDRESS,
+                        callData: contractInterface.encodeFunctionData(
+                            "baseTokenURI",
+                            []
+                        ),
+                    },
+                    {
+                        target: import.meta.env.VITE_NFT_CONTRACT_ADDRESS,
+                        callData: contractInterface.encodeFunctionData(
+                            "maxSupply",
+                            []
+                        ),
+                    },
+                    {
+                        target: import.meta.env.VITE_NFT_CONTRACT_ADDRESS,
+                        callData: contractInterface.encodeFunctionData(
+                            "mintPrice",
+                            []
+                        ),
+                    },
+                ];
+                console.log("calls", calls);
 
-    contract
-      .maxSupply()
-      .then((supply) => setMaxSupply(supply))
-      .catch((error) => console.error("error: ", error));
+                const results = await multicall.aggregate.staticCall(calls);
 
-    contract
-      .mintPrice()
-      .then((price) => setMintPrice(price))
-      .catch((error) => console.error("error: ", error));
-  }, []);
+                const resultsArray = JSON.parse(JSON.stringify(results))[1];
 
-  useEffect(() => {
-    if (!maxSupply || !baseTokenURI) return;
-    // const tokenIds = Array.from({ length: Number(maxSupply) }, (_, i) => i);
+                const nextTokenId = contractInterface.decodeFunctionResult(
+                    "nextTokenId",
+                    resultsArray[0]
+                );
+                const baseTokenURI = contractInterface.decodeFunctionResult(
+                    "baseTokenURI",
+                    resultsArray[1]
+                );
+                const maxSupply = contractInterface.decodeFunctionResult(
+                    "maxSupply",
+                    resultsArray[2]
+                );
+                const mintPrice = contractInterface.decodeFunctionResult(
+                    "mintPrice",
+                    resultsArray[3]
+                );
 
-    const tokenIds = [];
-    for (let i = 0; i < maxSupply; i++) {
-      tokenIds.push(i);
-    }
+                setNextTokenId(nextTokenId[0]);
+                setBaseTokenURI(baseTokenURI[0]);
+                setMaxSupply(maxSupply[0]);
+                setMintPrice(mintPrice[0]);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchContractData();
+    }, []);
 
-    const promises = tokenIds.map((id) => {
-      return fetch(`${baseTokenURI}${id}.json`)
-        .then((response) => response.json())
-        .then((data) => {
-          return data;
-        });
-    });
+    useEffect(() => {
+        if (!maxSupply || !baseTokenURI) return;
+        // const tokenIds = Array.from({ length: Number(maxSupply) }, (_, i) => i);
 
-    Promise.all(promises)
-      .then((responses) => {
-        const tokenMetaData = new Map();
-        responses.forEach((response, index) => {
-          tokenMetaData.set(index, response);
-        });
-        setTokenMetaData(tokenMetaData);
-      })
-      .catch((error) => console.error("error: ", error));
-  }, [baseTokenURI, maxSupply]);
-
-  //UseEffect to handle Minted event
-
-  useEffect(() => {
-    const contract = new Contract(
-      import.meta.env.VITE_NFT_CONTRACT_ADDRESS,
-      NFT_ABI,
-      getReadOnlyProvider()
-    );
-
-    const handleMintedNFT = (reciever, nftTokenId) => {
-      console.log(`NFT ${nftTokenId} has been minted to ${reciever}`);
-
-      contract
-        .nextTokenId()
-        .then((id) => setNextTokenId(id))
-        .catch((error) => {
-          console.log(error);
-        });
-    };
-
-    contract.on("Minted", handleMintedNFT);
-
-    return () => {
-      contract.off("Minted", handleMintedNFT);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (!userAddress || !isConnected || nextTokenId === null) return;
-    fetchUserNFTs();
-  }, [userAddress, isConnected, nextTokenId]);
-
-  const fetchUserNFTs = async () => {
-    try {
-      const contract = new Contract(
-        import.meta.env.VITE_NFT_CONTRACT_ADDRESS,
-        NFT_ABI,
-        getReadOnlyProvider()
-      );
-
-      const userOwnedNFTs = [];
-      for (let i = 0; i < nextTokenId; i++) {
-        const owner = await contract.ownerOf(i);
-        if (owner.toLowerCase() === userAddress.toLowerCase()) {
-          userOwnedNFTs.push(i);
+        const tokenIds = [];
+        for (let i = 0; i < maxSupply; i++) {
+            tokenIds.push(i);
         }
-      }
 
-      setUserNFTs(userOwnedNFTs);
-    } catch (error) {
-      console.error("Error fetching user NFTs: ", error);
-    }
-  };
+        const promises = tokenIds.map((id) => {
+            return fetch(`${baseTokenURI}${id}.json`)
+                .then((response) => response.json())
+                .then((data) => {
+                    return data;
+                });
+        });
 
-  const transferNFT = async (tokenId, recipient) => {
-    try {
-      const signer = await getEthersSigner(wagmiConfig); // Get connected wallet signer
-      if (!signer) throw new Error("Wallet not connected");
+        Promise.all(promises)
+            .then((responses) => {
+                const tokenMetaData = new Map();
+                responses.forEach((response, index) => {
+                    tokenMetaData.set(index, response);
+                });
+                setTokenMetaData(tokenMetaData);
+            })
+            .catch((error) => console.error("error: ", error));
+    }, [baseTokenURI, maxSupply]);
 
-      const contract = new Contract(
-        import.meta.env.VITE_NFT_CONTRACT_ADDRESS,
-        NFT_ABI,
-        signer
-      );
+    //UseEffect to handle Minted event
 
-      const tx = await contract.transferFrom(
-        await signer.getAddress(),
-        recipient,
-        tokenId
-      );
-      await tx.wait(); // Wait for transaction confirmation
+    useEffect(() => {
+        const contract = new Contract(
+            import.meta.env.VITE_NFT_CONTRACT_ADDRESS,
+            NFT_ABI,
+            getReadOnlyProvider()
+        );
 
-      console.log(`NFT ${tokenId} transferred to ${recipient}`);
-      setUserNFTs((prev) => prev.filter((id) => id !== tokenId)); // Remove transferred NFT from list
-    } catch (error) {
-      console.error("Transfer failed:", error);
-    }
-  };
+        const handleMintedNFT = (reciever, nftTokenId) => {
+            console.log(`NFT ${nftTokenId} has been minted to ${reciever}`);
 
-  return (
-    <appContext.Provider
-      value={{
-        nextTokenId,
-        maxSupply,
-        baseTokenURI,
-        tokenMetaData,
-        mintPrice,
-        userNFTs,
-        transferNFT,
-      }}
-    >
-      {children}
-    </appContext.Provider>
-  );
+            contract
+                .nextTokenId()
+                .then((id) => setNextTokenId(id))
+                .catch((error) => {
+                    console.log(error);
+                });
+        };
+
+        contract.on("Minted", handleMintedNFT);
+
+        return () => {
+            contract.off("Minted", handleMintedNFT);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (!userAddress || !isConnected || nextTokenId === null) return;
+        fetchUserNFTs();
+    }, [userAddress, isConnected, nextTokenId]);
+
+    const fetchUserNFTs = async () => {
+        try {
+            const multicall = new Contract(
+                import.meta.env.VITE_MULTICALL_CONTRACT_ADDRESS,
+                MULTCALL_ABI,
+                getReadOnlyProvider()
+            );
+
+            const contractInterface = new Interface(NFT_ABI);
+
+            const tokenIds = Array.from(
+                { length: Number(nextTokenId) },
+                (_, i) => i
+            );
+
+            const calls = tokenIds.map((id) => ({
+                target: import.meta.env.VITE_NFT_CONTRACT_ADDRESS,
+                callData: contractInterface.encodeFunctionData("ownerOf", [id]),
+            }));
+
+            const results = await multicall.aggregate.staticCall(calls);
+
+            const encodedResults = JSON.parse(JSON.stringify(results))[1];
+
+            const decodedResult = encodedResults.map(
+                (res) =>
+                    contractInterface.decodeFunctionResult("ownerOf", res)[0]
+            );
+
+            const isOwnedArray = decodedResult
+                .map((addr, i) =>
+                    addr.toLowerCase() === userAddress.toLowerCase() ? i : null
+                )
+                .filter((x) => x !== null);
+
+            setUserNFTs(isOwnedArray);
+        } catch (error) {
+            console.error("Error fetching user NFTs: ", error);
+        }
+    };
+
+    const transferNFT = async (tokenId, recipient) => {
+        try {
+            const signer = await getEthersSigner(wagmiConfig); // Get connected wallet signer
+            if (!signer) throw new Error("Wallet not connected");
+
+            const contract = new Contract(
+                import.meta.env.VITE_NFT_CONTRACT_ADDRESS,
+                NFT_ABI,
+                signer
+            );
+
+            const tx = await contract.transferFrom(
+                await signer.getAddress(),
+                recipient,
+                tokenId
+            );
+            await tx.wait(); // Wait for transaction confirmation
+
+            console.log(`NFT ${tokenId} transferred to ${recipient}`);
+            setUserNFTs((prev) => prev.filter((id) => id !== tokenId)); // Remove transferred NFT from list
+        } catch (error) {
+            console.error("Transfer failed:", error);
+        }
+    };
+
+    return (
+        <appContext.Provider
+            value={{
+                nextTokenId,
+                maxSupply,
+                baseTokenURI,
+                tokenMetaData,
+                mintPrice,
+                userNFTs,
+                transferNFT,
+            }}
+        >
+            {children}
+        </appContext.Provider>
+    );
 };
